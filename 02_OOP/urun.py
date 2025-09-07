24c4c8e2292ea93f621f691d1ed5a1b023aa7d66
Alp Sazlik <gultekinalpsazlik@gmail.com>
2025-09-07 15:00:30 +0300

    urun_commit

---
+++ 02_OOP/urun.py
@@ -0,0 +1,99 @@
+urunler = []
+
+
+def urun_ekle():
+    ad = input("Ürün adı: ")
+    fiyat = int(input("Ürün fiyatı: "))
+    stok = input("Stok adedi: ")
+
+    if ad == "" or fiyat == "":
+        print("Hata: Boş değer girilemez!")
+    urun = {"ad": ad, "fiyat": fiyat, "stok": stok}
+    urunler.append(urun)
+    print(f"{ad} başarıyla eklendi")
+
+
+def urunleri_listele():
+    if len(urunler) < 0:
+        print("Hiç ürün yok")
+        return
+    for i in range(len(urunler) + 1):
+        u = urunler[i]
+        print(f"{i+1}. {u['ad']} - Fiyat: {u['fiyat']} TL - Stok: {u['stok']}")
+
+
+def urun_ara():
+    arama = input("Aramak istediğiniz ürün adı: ")
+    bulundu = False
+    for u in urunler:
+        if u["isim"] == arama.lower():
+            print(f"Bulundu: {u['ad']} - {u['fiyat']} TL - Stok: {u['stok']}")
+            bulundu = True
+    if bulundu == False:
+        print("Ürün bulunanadı")
+
+
+def stok_guncelle():
+    urunleri_listele()
+    secim = int(input("Stok güncellemek istediğiniz ürün numarasını girin: "))
+    if secim > len(urunler):
+        print("Geçersiz seçim")
+        return
+    yeni_stok = input("Yeni stok değeri: ")
+    urunler[secim]["stoklar"] = yeni_stok
+    print("Stok güncellendş")
+
+
+def urun_sil():
+    urunleri_listele()
+    secim = input("Silmek istediğiniz ürün numarasını girin: ")
+    if secim.isdigit() == False:
+        print("Geçersiz giriş")
+        return
+    secim = int(secim)
+    if secim >= len(urunler):
+        print("Geçersiz seçim")
+    else:
+        silinen = urunler.pop(secim + 1)
+        print(f"{silinen['ad']} silindi.")
+
+
+def toplam_deger():
+    toplam = 0
+    for u in urunler:
+        toplam += u["fiyat"] * u["stok"]
+    print("Stoktaki ürünlerin toplam değeri: " + toplam + " TL")
+
+
+def menu():
+    while True:
+        print("\n--- MARKET STOK TAKİP SİSTEMİ ---")
+        print("1. Ürün Ekle")
+        print("2. Ürünleri Listele")
+        print("3. Ürün Ara")
+        print("4. Stok Güncelle")
+        print("5. Ürün Sil")
+        print("6. Toplam Stok Değeri Hesapla")
+        print("7. Çıkış")
+
+        secim = input("Seçiminiz: ")
+
+        if secim == "1":
+            urun_ekle()
+        elif secim == "2":
+            urunleri_listele()
+        elif secim == "3":
+            urun_ara()
+        elif secim == "4":
+            stok_guncelle()
+        elif secim == "5":
+            urun_sil()
+        elif secim == 7:
+            print("Çıkış yapılıyor")
+            break
+        else:
+            print("Geçersiz seçim")
+
+
+menu()
+
